939ed4ea8bb02a236aa3d6e8b8a736ee9ad43ff9
Shubham Kumar <131560016+Shubhamsit@users.noreply.github.com>
2023-12-30 14:15:57 +0000

    6th commit

--- 01_basics/08_date.js
+++ 01_basics/08_date.js
@@ -9,4 +9,17 @@ let myCreatedDate= new Date(2023,11,30)
 console.log(myCreatedDate.toDateString());
 
 
-let muTampStamp= dTe
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);// gives value in miliseconds from 1st jan 1970
+console.log(myCreatedDate.getTime());
+
+let newDate= new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+let z=newDate.toLocaleString('default',{
+    weekday: "long",
+
+}
+)
+console.log(z);
