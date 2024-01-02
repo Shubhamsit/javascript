d9f20ae1feba50fd455ee2be62ad663534de590f
Shubhamsit <shubhamsit31@gmail.com>
2024-01-02 22:39:42 +0530

    comit

---
+++ 05_iterations/three.js
@@ -0,0 +1,7 @@
+const mynum=[1,2,3,4,5,6,7,8,9,19]
+const newnums= mynum.filter((num)=>{      // returns the num which is greater than 4 and return an array of values
+   return  num>4
+
+
+}) 
+console.log(newnums);
--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -13,4 +13,50 @@ for (const key in myobject) {
     console.log(myobject[key]);  // gives value of keys
 }
 
+    /// for each loop
+
+    console.log("======for each looop=================");
+
+     const coding=["js","html","c++"]
+    coding.forEach(function (item){
+        console.log(item)
+    })
+
+    coding.forEach((item)=>{
+        console.log(item);
+    })
+
+    function printme(item)
+    {
+        console.log(item);
+    }
+    coding.forEach(printme) // only give reference of printme
+
+    coding.forEach((item,index,arrr)=>{
+        console.log(item,index,arrr);   // it also contains index and whole array 
+    })
+
+
+    //===================very important ----------------------
+
+    const mycoading=[
+        {
+            langname:"javascript",
+            size:"45"
+        },
+        {
+            langname:"python",
+            size:"77"
+        },
+        {
+            langname:"c++",
+            size:"776"
+        }
+
     
+    ]
+
+    mycoading.forEach((value)=>{
+        console.log(value.langname);
+        console.log(value.size);
+    })
